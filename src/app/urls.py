950377a0a73e4cb9e137ee792d6907e73293b633
delicalib <danil-nikonov-2017@mail.ru>
2025-07-26 01:52:58 +0300

    deleted needless

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,13 +2,11 @@ from django.urls import path, include
 from rest_framework import routers
 
 from app.views import AuthViewSet, ProfileViewSet
-from app.views.user import UserViewSet
 
 
 api_router = routers.DefaultRouter()
 api_router.register(r'auth', AuthViewSet, basename='auth')
 api_router.register(r'profile', ProfileViewSet, basename='profile')
-api_router.register(r'user', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(api_router.urls))
