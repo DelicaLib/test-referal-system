3097e0835f043456ea2fde28b535fac95b0229c3
delicalib <danil-nikonov-2017@mail.ru>
2025-12-24 18:38:36 +0300

    feature: k8s

--- src/referal_system/settings.py
+++ src/referal_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-b&aag@7tvh3k^100w#a_-$uvi+ayukx3$tgbrq6q3d6=mmn$ao
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
